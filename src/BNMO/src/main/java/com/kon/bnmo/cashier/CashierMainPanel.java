515db52d97146205cc9baceaf3eadc271c117914
Zaki <zakiamanullah215@gmail.com>
2023-05-07 15:49:55 +0700

    Clean: Refactor

--- src/BNMO/src/main/java/com/kon/bnmo/cashier/CashierMainPanel.java
+++ src/BNMO/src/main/java/com/kon/bnmo/cashier/CashierMainPanel.java
@@ -1,19 +1,34 @@
 package com.kon.bnmo.cashier;
 
+import com.kon.bnmo.items.Item;
+import com.kon.bnmo.items.ItemHolder;
+import com.kon.bnmo.sistembarang.SistemBarang;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+
+import java.util.List;
 
 public class CashierMainPanel extends VBox {
     private Catalogue catalogue;
     private Button restockButton;
     private Cashier parent;
+    private ItemHolder availableItems;
+    private SistemBarang sistemBarang;
 
-    public CashierMainPanel(Cashier parent) {
+    public CashierMainPanel(Cashier parent, ItemHolder availableItems) {
         this.parent = parent;
-        this.catalogue = new Catalogue(this);
+        this.catalogue = new Catalogue(this, availableItems);
         this.catalogue.getvBox().prefWidthProperty().bind(this.widthProperty());
+        this.availableItems = availableItems;
         this.restockButton = new Button("Restock");
+//        this.sistemBarang = new SistemBarang()
+        this.restockButton.setOnAction(this::openSistemBarang);
         this.getChildren().addAll(this.catalogue, this.restockButton);
+    }
+
+    private void openSistemBarang(ActionEvent actionEvent) {
+
     }
 
     public Catalogue getCatalogue() {
@@ -38,5 +53,13 @@ public class CashierMainPanel extends VBox {
 
     public void setCashier(Cashier parent) {
         this.parent = parent;
+    }
+
+    public ItemHolder getAvailableItems() {
+        return availableItems;
+    }
+
+    public void setAvailableItems(ItemHolder availableItems) {
+        this.availableItems = availableItems;
     }
 }
--- src/BNMO/src/main/java/com/kon/bnmo/cashier/NewCustomer.java
+++ src/BNMO/src/main/java/com/kon/bnmo/cashier/NewCustomer.java
@@ -1,5 +1,11 @@
 package com.kon.bnmo.cashier;
 
+import com.kon.bnmo.customers.CustomerHolder;
+import com.kon.bnmo.customers.MemberModel;
+import com.kon.bnmo.customers.Person;
+import com.kon.bnmo.customers.VIPModel;
+import com.kon.bnmo.items.Item;
+import com.kon.bnmo.items.ItemHolder;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
@@ -10,15 +16,27 @@ public class NewCustomer extends Tab {
     private ChoiceBox<String> memberList;
     private TextField nonMember;
     private final Button enterButton;
+    private CustomerHolder personList;
+    private ItemHolder availableItems;
 
-    public NewCustomer(List<String> memberList) {
+    public NewCustomer(CustomerHolder personList, ItemHolder availableItems) {
 
         super("Cashier: New Customer");
+        this.personList = personList;
+        this.availableItems = availableItems;
 
         // Add the members to the dropdown selection
         this.memberList = new ChoiceBox<>();
-        for (String member : memberList) {
-            this.memberList.getItems().add(member);
+        for (Person p : this.personList.getItemList()) {
+            if (p.getType().equals("member") || p.getType().equals("vip")) {
+                if (p.getType().equals("member")) {
+                    MemberModel memberPerson = (MemberModel) p;
+                    this.memberList.getItems().add(memberPerson.getName());
+                } else if (p.getType().equals("vip")) {
+                    VIPModel vipPerson = (VIPModel) p;
+                    this.memberList.getItems().add(vipPerson.getName());
+                }
+            }
         }
         this.memberList.getItems().add("Other");
         this.memberList.setValue("Choose Member");
@@ -44,7 +62,7 @@ public class NewCustomer extends Tab {
         } else {
             value = this.nonMember.getText();
         }
-        Cashier cashierPage = new Cashier(value);
+        Cashier cashierPage = new Cashier(value, this.availableItems, this.personList);
         this.getTabPane().getTabs().add(cashierPage);
         this.getTabPane().getSelectionModel().select(cashierPage);
         TabPane tabPane = getTabPane();
@@ -72,5 +90,21 @@ public class NewCustomer extends Tab {
 
     public void setNonMember(TextField nonMember) {
         this.nonMember = nonMember;
+    }
+
+    public CustomerHolder getPersonList() {
+        return personList;
+    }
+
+    public void setPersonList(CustomerHolder personList) {
+        this.personList = personList;
+    }
+
+    public ItemHolder getAvailableItems() {
+        return availableItems;
+    }
+
+    public void setAvailableItems(ItemHolder availableItems) {
+        this.availableItems = availableItems;
     }
 }
